b2b40bcc09515aa63bd0c35fd6337f74608bfda0
DanielGG1618 <daniel.gevorgyan25@gmail.com>
2024-09-28 15:28:58 +0300

    pulling hyprland newest version

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }: {
+{ inputs, system, ... }: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    package = inputs.hyprland.packages.${system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
   };
   
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   xdg.portal = {
     enable = true;
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    extraPortals = [inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland];
   };
 }
