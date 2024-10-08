beb92053c7e691a63ccf3ef48d9675182c0b6270
DanielGG1618 <daniel.gevorgyan25@gmail.com>
2024-10-08 22:27:10 +0300

    yank and paste with system clipboard in nvim

--- home/modules/nvim/lua/remap.lua
+++ home/modules/nvim/lua/remap.lua
@@ -11,3 +11,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+yg_')
+vim.keymap.set('n', '<leader>y', '"+y')
+
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
