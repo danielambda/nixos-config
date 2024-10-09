f59d009b9a8169c3be59603d564609a59447584d
DanielGG1618 <daniel.gevorgyan25@gmail.com>
2024-10-09 17:50:29 +0300

    fixed snippets keybinds

--- home/modules/nvim/lua/plugins/cmp.lua
+++ home/modules/nvim/lua/plugins/cmp.lua
@@ -1,5 +1,6 @@
-local cmp = require'cmp'
-local luasnip = require'luasnip'
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+luasnip.config.setup {}
 
 cmp.setup {
     snippet = {
@@ -8,26 +9,22 @@ cmp.setup {
         end,
     },
     completion = { completeopt = 'menu,menuone,noinsert' },
+
     mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
         ['<C-y>'] = cmp.mapping.confirm { select = true },
-        ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-            end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            end
-        end, { 'i', 's' })
+        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
     },
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-        { name = 'path' }
-    }
+        { name = 'path' },
+    },
 }
--- home/modules/nvim/lua/plugins/luasnip.lua
+++ home/modules/nvim/lua/plugins/luasnip.lua
@@ -3,11 +3,23 @@ local snippet = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
 
+vim.keymap.set({ 'i', 's' }, '<C-l>', function ()
+    if luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+    end
+end, { silent = true })
+
+vim.keymap.set({ 'i', 's' }, '<C-h>', function ()
+    if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+    end
+end, { silent = true })
+
 local configByProfile = {
     ['obsidian'] = function ()
         luasnip.add_snippets('markdown', {
             snippet('\\sum', {
-                t'\\sum_{', i(1), t'}^{', i(2), t'}'
+                t'\\sum_{', i(1, 'i = 0'), t'}^{', i(2, '\\infty'), t'}'
             })
         })
     end,
