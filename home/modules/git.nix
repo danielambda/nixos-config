6526fcfd5d50d05031904ee64e21ede5a82d970b
danielambda <daniel.gevorgyan25@gmail.com>
2024-11-02 05:17:45 +0300

    probably git auth fix

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,11 +1,12 @@
-{}# {
-#   programs.git = {
-#     enable = true;
-#     userName = "danielambda";
-#     userEmail = "daniel.gevorgyan25@gmail.com";
-#
-#     extraConfig = {
-#       init.defaultBranch = "main";
-#     };
-#   };
-# }
+{ lib, pkgs, ... }: {
+  programs.git = {
+    enable = true;
+    userName = "danielambda";
+    userEmail = "daniel.gevorgyan25@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "!${lib.getExe pkgs.gh} auth git-credential";
+    };
+  };
+}
