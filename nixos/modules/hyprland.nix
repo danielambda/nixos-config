5bb424f043410896830f206f0508af3b081ed203
danielambda <daniel.gevorgyan25@gmail.com>
2025-03-16 23:06:18 +0300

    hyprland: added nix cache

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,11 +1,16 @@
 { inputs, system, ... }: {
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
     package = inputs.hyprland.packages.${system}.hyprland;
     portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
   };
-  
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   xdg.portal = {
