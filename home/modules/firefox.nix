bd1b34efe728d22b6e7a9dd091e52c4b20ac7037
danielambda <daniel.gevorgyan25@gmail.com>
2025-04-22 04:08:00 +0300

    firefox: remove pwa

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -1,9 +1,6 @@
-{ pkgs, ... }: {
+{
   programs.firefox = {
     enable = true;
     languagePacks = ["en-US" "ru"];
-    nativeMessagingHosts = [pkgs.firefoxpwa];
   };
-
-  home.packages = [pkgs.firefoxpwa];
 }
