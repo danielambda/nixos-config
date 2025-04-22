bc6db74ec2f717f41495cd89a992f0d120099fdc
danielambda <daniel.gevorgyan25@gmail.com>
2025-04-22 04:07:44 +0300

    kitty: lingatures are back

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -7,7 +7,7 @@
       dynamic_background_opacity = true;
       allow_remote_control = true;
       map = "ctrl+shift+m launch --type overlay kitten @ set-background-opacity --toggle 1";
-      disable_ligatures = "always";
+      # disable_ligatures = "always";
     };
 
     shellIntegration.enableZshIntegration = true;
