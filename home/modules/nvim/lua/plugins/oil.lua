d3fcfe87b8b5725737ba29f45b29064c9b777cbb
danielambda <daniel.gevorgyan25@gmail.com>
2025-06-22 01:47:22 +0300

    nvim: close deleted files via oil.nvim

--- home/modules/nvim/lua/plugins/oil.lua
+++ home/modules/nvim/lua/plugins/oil.lua
@@ -1,2 +1,34 @@
 require'oil'.setup()
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- close deleted files via oil.nvim
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(args)
+    local parse_url = function(url)
+      return url:match("^.*://(.*)$")
+    end
+
+    if args.data.err then
+      return
+    end
+
+    for _, action in ipairs(args.data.actions) do
+      if action.type == "delete" and action.entry_type == "file" then
+        local path = parse_url(action.url)
+        local bufnr = vim.fn.bufnr(path)
+        if bufnr == -1 then
+          return
+        end
+
+        local winnr = vim.fn.win_findbuf(bufnr)[1]
+        if not winnr then
+          vim.cmd("bw " .. bufnr)
+          return
+        end
+
+        vim.fn.win_execute(winnr, "bfirst | bw " .. bufnr)
+      end
+    end
+  end,
+})
