fd3b28da4dbc16b0e8305690d7a53076f977c5c6
danielambda <daniel.gevorgyan25@gmail.com>
2026-04-08 13:01:40 +0300

    keybind for toggling lsplines

--- modules/features/nvim/lua/plugins/lsp_lines-nvim.lua
+++ modules/features/nvim/lua/plugins/lsp_lines-nvim.lua
@@ -4,5 +4,11 @@ return {
   after = function()
     vim.diagnostic.config({ virtual_text = true })
     vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+    vim.keymap.set(
+      "",
+      "<Leader>l",
+      require("lsp_lines").toggle,
+      { desc = "Toggle lsp_lines" }
+    )
   end
 }
